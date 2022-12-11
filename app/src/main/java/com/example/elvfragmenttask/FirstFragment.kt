b73b880bce627c2f386bfbc784984663adc51c73
Elvira-rav <elvira-rav@yandex.ru>
2022-12-11 23:51:41 +0500

    first

---
+++ app/src/main/java/com/example/elvfragmenttask/FirstFragment.kt
@@ -0,0 +1,18 @@
+package com.example.elvfragmenttask
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class FirstFragment : Fragment(R.layout.fragment_first) {
+
+    companion object {
+       @JvmStatic
+        fun newInstance() =
+            FirstFragment()
+                }
+            }
+
---
+++ app/src/main/java/com/example/elvfragmenttask/SecondFragment.kt
@@ -0,0 +1,15 @@
+package com.example.elvfragmenttask
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class SecondFragment : Fragment(R.layout.fragment_second) {
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            SecondFragment()
+    }
+}
