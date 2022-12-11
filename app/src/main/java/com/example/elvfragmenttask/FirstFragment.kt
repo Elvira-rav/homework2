84795cae20c84474838a9634e8c7dd8b7ff2b113
Elvira-rav <elvira-rav@yandex.ru>
2022-12-12 02:13:09 +0500

    home_fragment

--- app/src/main/java/com/example/elvfragmenttask/FirstFragment.kt
+++ app/src/main/java/com/example/elvfragmenttask/FirstFragment.kt
@@ -5,9 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 
 class FirstFragment : Fragment(R.layout.fragment_first) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val button = view.findViewById<Button>(R.id.buttSwitch)
+        button.setOnClickListener {
+            (requireActivity() as MainActivity).switchToSecondFragment()
+        }
+    }
+
 
     companion object {
        @JvmStatic
